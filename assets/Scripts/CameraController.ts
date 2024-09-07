00910c4aa38b9d72f2a99508bcf27f2964c11989
amadeus711 <952937570@qq.com>
2024-09-07 12:32:37 +0800

    feat: add camera move

--- assets/Scripts/CameraController.ts
+++ assets/Scripts/CameraController.ts
@@ -1,8 +1,20 @@
-import { _decorator, Component, EventTouch, Input, input, Node } from 'cc';
+import { _decorator, Component, EventTouch, Input, input, math, Node, Rect, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('CameraController')
 export class CameraController extends Component {
+  /**
+   * 相机移动范围的最小值
+   */
+  @property(Vec3)
+  public moveMin: Vec3 = new Vec3(-10, 0, 0);
+
+  /**
+   * 相机移动范围的最大值
+   */
+  @property(Vec3)
+  public moveMax: Vec3 = new Vec3(10, 10, 0);
+
   start() {
     input.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
     input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
@@ -15,10 +27,12 @@ export class CameraController extends Component {
     // moveScale 用于控制移动速度，避免移动过快
     const moveScale = 0.01;
     const curPos = this.node.position;
+    const moveX = math.clamp(curPos.x - event.getDeltaX() * moveScale, this.moveMin.x, this.moveMax.x);
+    const moveY = math.clamp(curPos.y - event.getDeltaY() * moveScale, this.moveMin.y, this.moveMax.y);
     // 为了符合移动端的滑动方向，这里需要将移动距离取反
     this.node.setPosition(
-      curPos.x - event.getDeltaX() * moveScale,
-      curPos.y - event.getDeltaY() * moveScale,
+      moveX,
+      moveY,
       curPos.z
     );
   }
